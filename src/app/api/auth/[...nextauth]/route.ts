0d89dfc9e0cf3563040481a1eaa9c7f04b4a8c51
Pham Thi Huong Quynh <pthquynh2001@gmail.com>
2024-01-17 00:46:30 +0700

    update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,22 @@
-// import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
-// import CredentialsProvider from 'next-auth/providers/credentials';
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {},
 
-// const authOptions = {
-//   providers: [
-//     CredentialsProvider({
-//       name: 'Credentials',
-//       credentials: {},
-
-//       async authorize(credentials) {
-//         const user = { id: '1' };
-//         return user;
-//       },
-//     }),
-//   ],
-//   session: {
-//     strategy: 'jwt',
-//   },
-//   secret: process.env.NEXTAUTH_SECRET,
-//   pages: {
-//     login: '/auth/login',
-//   },
-// };
-
-// const handler = NextAuth(authOptions);
-// export { handler as GET, handler as POST };
+      async authorize(credentials) {
+        const user = { id: '1' };
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    login: '/auth/login',
+  },
+};
